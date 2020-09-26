f71f5bec29315dea9e32b442b3bb1b9bc792cf54
huangqian8 <bluefire1982@gmail.com>
2020-09-26 19:58:33 +0800

    fix & update

--- script.sh
+++ script.sh
@@ -179,21 +179,28 @@ EOF
 git clone https://github.com/vernesong/OpenClash.git package-temp
 mv -f package-temp/luci-app-openclash package/lean/
 cat >> .config <<EOF
-CONFIG_PACKAGE_luci-app-openclash=y
 CONFIG_LIBCURL_COOKIES=y
 CONFIG_LIBCURL_FILE=y
 CONFIG_LIBCURL_FTP=y
 CONFIG_LIBCURL_HTTP=y
+CONFIG_LIBCURL_MBEDTLS=y
 CONFIG_LIBCURL_NO_SMB="!"
-CONFIG_LIBCURL_OPENSSL=y
 CONFIG_LIBCURL_PROXY=y
 CONFIG_PACKAGE_bash=y
 CONFIG_PACKAGE_ca-bundle=y
+CONFIG_PACKAGE_coreutils=y
 CONFIG_PACKAGE_coreutils-nohup=y
 CONFIG_PACKAGE_curl=y
+CONFIG_PACKAGE_iptables-mod-tproxy=y
+CONFIG_PACKAGE_kmod-ip6tables=y
+CONFIG_PACKAGE_kmod-ipt-tproxy=y
+CONFIG_PACKAGE_kmod-nf-ipt6=y
+CONFIG_PACKAGE_kmod-nf-reject6=y
 CONFIG_PACKAGE_libcurl=y
+CONFIG_PACKAGE_libmbedtls=y
 CONFIG_PACKAGE_libncurses=y
 CONFIG_PACKAGE_libreadline=y
+CONFIG_PACKAGE_luci-app-openclash=y
 CONFIG_PACKAGE_terminfo=y
 EOF
 rm -rf package-temp
