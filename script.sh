039dac4fdfdb973a632f7142b738ddd971d46564
zouchanggan <61627503+zouchanggan@users.noreply.github.com>
2022-08-22 13:44:50 +0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -7,7 +7,6 @@ rm -rf files/etc/modules.d/wireless_enable
 rm -rf files/etc/config/amlogic
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.1.10/g' openwrt/package/base-files/files/bin/config_generate
-
 # Clear the login password
 sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-settings/files/zzz-default-settings
 # Add luci-app-openclash
