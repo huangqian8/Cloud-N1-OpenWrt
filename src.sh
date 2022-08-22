a9a333b4ddb430fcab66248ea9d4ccf09196d5f6
zouchanggan <61627503+zouchanggan@users.noreply.github.com>
2022-08-22 12:51:48 +0800

    Update src.sh

--- src.sh
+++ src.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd openwrt
 # Add a feed source
-echo 'src-git SSRplus https://github.com/fw876/helloworld' >>feeds.conf.default
-echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2' >>feeds.conf.default
+echo "src-git SSRplus https://github.com/fw876/helloworld.git" >> ./feeds.conf.default
+echo "src-git kenzo https://github.com/kenzok8/openwrt-packages" >> ./feeds.conf.default
+echo "src-git small https://github.com/kenzok8/small" >> ./feeds.conf.default
