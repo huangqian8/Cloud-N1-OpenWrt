602bd5a5abe830187258e11b1fe56eca46fc8bb5
zouchanggan <61627503+zouchanggan@users.noreply.github.com>
2022-08-22 12:41:11 +0800

    Create src.sh

---
+++ src.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cd openwrt
+# Add a feed source
+echo 'src-git SSRplus https://github.com/fw876/helloworld' >>feeds.conf.default
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2' >>feeds.conf.default
