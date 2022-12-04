eabbcdb8b54b34207e1250cea17c1551255992df
zouchanggan <61627503+zouchanggan@users.noreply.github.com>
2022-12-04 11:58:47 +0800

    Update src.sh

--- src.sh
+++ src.sh
@@ -4,3 +4,4 @@ cd openwrt
 echo "src-git SSRplus https://github.com/fw876/helloworld.git" >> ./feeds.conf.default
 echo "src-git kenzo https://github.com/kenzok8/openwrt-packages" >> ./feeds.conf.default
 echo "src-git small https://github.com/kenzok8/small" >> ./feeds.conf.default
+echo "src-git oaf https://github.com/destan19/OpenAppFilter.git" >> ./feeds.conf.default
