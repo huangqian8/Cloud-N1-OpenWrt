32f653999570844244bfd63784751437288683eb
LwjiVD2t <244722+Lwjivd@users.noreply.github.com>
2023-10-04 10:08:29 +0800

    Update config.sh

--- config.sh
+++ config.sh
@@ -167,9 +167,9 @@ CONFIG_PACKAGE_lua-cjson=y
 CONFIG_PACKAGE_lua-maxminddb=y
 CONFIG_PACKAGE_luasocket=y
 # CONFIG_PACKAGE_luci-app-accesscontrol is not set
-CONFIG_PACKAGE_luci-app-adguardhome=y
+CONFIG_PACKAGE_luci-app-adguardhome=n
 CONFIG_PACKAGE_luci-app-aliyundrive-webdav=y
-CONFIG_PACKAGE_luci-app-amlogic=y
+CONFIG_PACKAGE_luci-app-amlogic=n
 # CONFIG_PACKAGE_luci-app-arpbind is not set
 # CONFIG_PACKAGE_luci-app-bypass_INCLUDE_ShadowsocksR_Libev_Client is not set
 # CONFIG_PACKAGE_luci-app-bypass_INCLUDE_ShadowsocksR_Libev_Server is not set
@@ -203,7 +203,7 @@ CONFIG_PACKAGE_luci-app-vssr_INCLUDE_Kcptun=y
 CONFIG_PACKAGE_luci-app-webadmin=y
 CONFIG_PACKAGE_luci-app-zerotier=y
 CONFIG_PACKAGE_luci-i18n-aliyundrive-webdav-zh-cn=y
-CONFIG_PACKAGE_luci-i18n-amlogic-zh-cn=y
+CONFIG_PACKAGE_luci-i18n-amlogic-zh-cn=n
 CONFIG_PACKAGE_luci-i18n-docker-zh-cn=n
 CONFIG_PACKAGE_luci-i18n-dockerman-zh-cn=n
 CONFIG_PACKAGE_luci-i18n-openvpn-server-zh-cn=y
