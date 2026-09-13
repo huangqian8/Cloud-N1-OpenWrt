a2f01915bf769969958af2b334ba0df00a5106b2
huangqian8 <bluefire1982@gmail.com>
2026-09-13 22:02:32 +0800

    fix(ci): order feed customization safely

--- script.sh
+++ script.sh
@@ -24,8 +24,6 @@ rm -rf theme-temp/luci-theme-opentomcat/LICENSE
 rm -rf theme-temp/luci-theme-opentomcat/README.md
 mv -f theme-temp/luci-theme-opentomcat package/lean/
 rm -rf theme-temp
-default_theme='opentomcat'
-sed -i "s/bootstrap/$default_theme/g" feeds/luci/modules/luci-base/root/etc/config/luci
 
 # Add luci-app-amlogic
 clone_retry https://github.com/ophub/luci-app-amlogic.git package-temp/luci-app-amlogic
